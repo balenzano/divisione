2710b3518700dc0a0b26ee4960069042ffee113f
balenzano <123358270+balenzano@users.noreply.github.com>
2023-01-23 13:48:59 +0100

    Add files via upload

---
+++ es divisione.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int n1=0;
+	int n2=0;
+	int DIV=0;
+cout<<"inserisci il primo numero";
+cin>>n1;
+cout<<"inserisci il secondo numero";
+cin>>n2;
+DIV=(n1/n2)
+cout<<"il risultato della divisione e/'";
+cin>>DIV;
+return 0;
+}
